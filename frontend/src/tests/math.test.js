b9825012f6d3ccb9e1cb602fd601a92e06a09378
Javier Granados <hola@javiergranados.dev>
2023-10-19 17:42:42 +0200

    Add more tests and fix add function

--- frontend/src/tests/math.test.js
+++ frontend/src/tests/math.test.js
@@ -4,9 +4,31 @@ import { add } from '../math'
 
 describe('Func add', () => {
   test('summarize all number values in an array', () => {
-    const sum = add([1,2,3])
-    expect(sum).equals(6)
+    // Arrange
+    const numbers = [1, 2, 3]
+    const expectedResult = 6
+
+    // Act
+    const sum = add(numbers)
+
+    // Assert
+    expect(sum).toBe(expectedResult)
+  })
+
+  test('returns NaN if one of the values is not a number', () => {
+    const numbers = [1, 2, "string"]
+
+    const sum = add(numbers)
+
+    expect(sum).toBeNaN()
+  })
+
+  test('summarize all numeric string values in an array', () => {
+    const numbers = ["1", "2", "3"]
+    const expectedResult = 6
+
+    const sum = add(numbers)
+
+    expect(sum).toBe(expectedResult)
   })
 })
-
-// ver diferencias equals y tobe en vistest
