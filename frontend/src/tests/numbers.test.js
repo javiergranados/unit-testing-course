d30b336b065f6f914209118545d2f4dbb1e468f6
Javier Granados <hola@javiergranados.dev>
2023-11-08 17:40:52 +0100

    Implement unit testing for cleanNumbers function

--- frontend/src/tests/numbers.test.js
+++ frontend/src/tests/numbers.test.js
@@ -1,5 +1,5 @@
 import { test, expect, describe } from 'vitest'
-import { transformToNumber } from '../util/numbers'
+import { cleanNumbers, transformToNumber } from '../util/numbers'
 
 
 describe('transformToNumber()', () => {
@@ -26,5 +26,32 @@ describe('transformToNumber()', () => {
     const result = transformToNumber(number)
 
     expect(result).toBe(number)
+  })
+})
+
+describe('cleanNumbers()', () => {
+  test('numeric strings are transformed to numbers', () => {
+    const arg = ['1', '2', '3']
+
+    const result = cleanNumbers(arg)
+
+    expect(result[0]).toBeTypeOf('number')
+  })
+
+  test('receives a numeric strings array and returns an array of numbers', () => {
+    const arg = ['1', '2', '3']
+    const expectedResult = [1, 2, 3]
+
+    const result = cleanNumbers(arg)
+
+    expect(result).toEqual(expectedResult)
+  })
+
+  test('throws an error if the array provided has at least one empty string', () => {
+    const arg = ['', '2', '3']
+
+    const resultFn = () => cleanNumbers(arg)
+
+    expect(resultFn).toThrow()
   })
 })
