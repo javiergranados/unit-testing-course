e239ea33a7a6d2da644c5aeee4bb085cbc1f6f00
Javier Granados <hola@javiergranados.dev>
2023-11-09 11:57:14 +0100

    Cleanup test file using hooks

--- hooks/hooks.test.js
+++ hooks/hooks.test.js
@@ -1,47 +1,37 @@
-import { describe, test, expect } from 'vitest';
-
+import { describe, test, expect, beforeEach } from 'vitest';
 import { User } from './hooks';
 
+const arg = 'test@email.com';
+let user;
+
 describe('Class User', () => {
+  beforeEach(() => {
+    user = new User(arg);
+  })
+
   test('email is updated correctly', () => {
-    const arg = 'test@email.com';
     const arg2 = 'test2@email.com';
 
-    const user = new User(arg);
     user.updateEmail(arg2);
 
     expect(user.email).toBe(arg2);
   });
 
   test('the object has the property email', () => {
-    const arg = 'test@test.com';
-
-    const user = new User(arg);
-
     expect(user).toHaveProperty('email');
   });
 
   test('the email is stored properly', () => {
-    const arg = 'test@email.com';
-
-    const user = new User(arg);
-
     expect(user.email).toBe(arg);
   });
 
   test('the email is cleared properly', () => {
-    const arg = 'test@email.com';
-
-    const user = new User(arg);
     user.clearEmail();
 
     expect(user.email).toBe('');
   });
 
   test('the property email is still after clear its value', () => {
-    const arg = 'test@email.com';
-
-    const user = new User(arg);
     user.clearEmail();
 
     expect(user).toHaveProperty('email');
