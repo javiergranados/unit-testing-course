c510223582d74a16789e0a530ec3ff2ebd7ffb82
Javier Granados <hola@javiergranados.dev>
2023-11-22 12:56:13 +0100

    Implement more tests for showError function

--- dom/tests/dom.test.js
+++ dom/tests/dom.test.js
@@ -1,24 +1,43 @@
-import { vi, describe, it, expect } from 'vitest';
+import { vi, describe, it, expect, beforeEach } from 'vitest';
 import fs from 'fs'
 import path from 'path'
 import { Window } from 'happy-dom'
 import { showError } from '../util/dom';
 
 const htmlDocPath = path.join(process.cwd(), 'index.html')
-const htmlDocumentcontent = fs.readFileSync(htmlDocPath).toString()
+const htmlDocumentContent = fs.readFileSync(htmlDocPath).toString()
 
 const window = new Window()
 const document = window.document
-document.write(htmlDocumentcontent)
+document.write(htmlDocumentContent)
 
 vi.stubGlobal('document', document)
 
 describe('showError()', () => {
+  beforeEach(() => {
+    document.body.innerHTML = '';
+    document.write(htmlDocumentContent);
+  });
+
   it('should add an error paragraph to the id="errors" element', () => {
     showError('test')
-
     const errorsEl = document.getElementById('errors')
 
     expect(errorsEl.firstElementChild).not.toBeNull()
   })
+
+  it('should not contain an error paragraph initially', () => {
+    const errorsEl = document.getElementById('errors')
+
+    expect(errorsEl.firstElementChild).toBeNull()
+  })
+
+  it('should output the provided message in the error paragraph', () => {
+    const errorMessage = 'Test';
+
+    showError(errorMessage);
+    const errorsEl = document.getElementById('errors');
+
+    expect(errorsEl.firstElementChild.textContent).toBe(errorMessage);
+  });
 });
